5c761f4ad3e1099e3b048eac09bba2c65d36b16f
Ben Rosen <benjamin.j.rosen@gmail.com>
2025-05-22 13:19:45 -0400

    chore: update environment

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     environments = {
       inputs.nixpkgs.follows = "nixpkgs";
-      url = "github:tymbalodeon/environments/rewrite?dir=src";
+      url = "github:tymbalodeon/environments?dir=src";
     };
 
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
